3a8c99f048b4e5a4fac9d2b43137a8da736a0748
Bruno Caxito <bcaxito@gmail.com>
2026-01-04 23:05:30 -0300

    Add GitHub actions

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
-  base: './',
+  plugins: [react()],
+  base: '/PayFlow/',
   publicDir: 'data',
 })
